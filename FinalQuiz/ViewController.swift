1ffa9b0a81435c62418c6b20cc19e5b32464c695
Anthony Pound <anthonypound@MBP-Anthony-Pound-Staff.local>
2017-04-26 15:47:07 -0700

    Completed Quiz app

--- FinalQuiz/ViewController.swift
+++ FinalQuiz/ViewController.swift
@@ -9,15 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = [
+        "What is Mr. Pound's favorite soccer team?",
+        "How far is the moon from Earth?",
+        "What is the value of Pi to 3 decimal places?"
+    ]
+    let answers: [String] = [
+        "Liverpool",
+        "239,000 miles",
+        "3.141"
+    ]
+    var currentQuestionIndex: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        questionLabel.text = questions[currentQuestionIndex]
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+    }
+    
+    
+    @IBAction func showAnswer(_ sender: UIButton) {
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
     }
 
 
